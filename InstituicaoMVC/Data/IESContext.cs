7f76755db01c0e602cfec22a979100ec6a7bb018
Gabriel Julian <gluisjulian@gmail.com>
2024-02-23 09:23:52 -0300

    Conclusao Cap4

---
+++ InstituicaoMVC/Data/IESContext.cs
@@ -0,0 +1,31 @@
+﻿using InstituicaoMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InstituicaoMVC.Data
+{
+    public class IESContext : DbContext
+    {
+
+        public IESContext(DbContextOptions<IESContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Instituicao> Instituicoes { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Departamento>().ToTable("Departamento");
+        }
+
+
+        //Caso queira usar dessa forma pode configurar o acesso ao banco na classe de contexto
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=IESCasaDoCodigo;Trusted_Connection=True;MultipleActiveResultSets=true");
+        //}
+    }
+}
